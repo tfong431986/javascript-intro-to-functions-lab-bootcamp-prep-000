8643f0967c2fc0c397457e91db74ee7b54b9b52a
tfong4386 <tfong4386@gmail.com>
2019-03-11 08:58:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (var string.toLowerCase() === string) {
+  if (string.toLowerCase() === string) {
     return "I can't hear you!"
-  } else if (var uppercase.toUpperCase() === string) {
+  } else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   } else {
     return "I love you, too."
